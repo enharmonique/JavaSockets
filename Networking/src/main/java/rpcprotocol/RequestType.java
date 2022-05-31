c20955181c3b16f8822e869544d5efef3916f562
Tepfenhart Barbara <83082321+enharmonique@users.noreply.github.com>
2022-05-31 20:20:18 +0300

    Delete Networking directory

--- Networking/src/main/java/rpcprotocol/RequestType.java
+++
@@ -1,5 +0,0 @@
-package rpcprotocol;
-
-public enum RequestType {
-    LOGIN, LOGOUT, ADD_DONATION, ADD_DONOR, GET_CHARITY_CASES, SEARCH_DONORS;
-}
--- Networking/src/main/java/rpcprotocol/ResponseType.java
+++
@@ -1,5 +0,0 @@
-package rpcprotocol;
-
-public enum ResponseType {
-    OK, ERROR, UPDATE, NEW_DONATION, NEW_DONOR, GET_CHARITY_CASES, FOUND_DONORS;
-}
