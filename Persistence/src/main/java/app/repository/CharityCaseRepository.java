42a81365581193b7d6676d77d69ed580e52c1dc8
Tepfenhart Barbara <83082321+enharmonique@users.noreply.github.com>
2022-05-31 20:20:28 +0300

    Delete Persistence directory

--- Persistence/src/main/java/app/repository/CharityCaseRepository.java
+++
@@ -1,7 +0,0 @@
-package app.repository;
-
-import app.model.CharityCase;
-
-public interface CharityCaseRepository extends ICrudRepository<Integer, CharityCase> {
-    CharityCase findByName(String string);
-}
