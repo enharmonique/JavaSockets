2d3d212419215e5b5487e36909ecd3252c7f92a3
Tepfenhart Barbara <83082321+enharmonique@users.noreply.github.com>
2022-05-31 20:20:51 +0300

    Delete Services directory

--- Services/src/main/java/app/services/IAppObserver.java
+++
@@ -1,9 +0,0 @@
-package app.services;
-
-import app.model.Donation;
-import app.model.Donor;
-
-public interface IAppObserver {
-    void donationAdded(Donation donation) throws AppException;
-    void donorAdded(Donor donor) throws AppException;
-}
